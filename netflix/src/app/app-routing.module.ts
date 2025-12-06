bee4bc921147a08916dae951e2c0db35078fefe9
abhi <abhibezavada@gmail.com>
2025-12-06 11:59:01 +0530

    update

--- netflix/src/app/app-routing.module.ts
+++ netflix/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { IphoneComponent } from './navigatingcomponents/iphone/iphone.component';
+import { AndroidComponent } from './navigatingcomponents/android/android.component';
+import { HelpComponent } from './navigatingcomponents/help/help.component';
+import { SigninComponent } from './navigatingcomponents/signin/signin.component';
+import { CompanyComponent } from './navigatingcomponents/company/company.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:WelcomeComponent},
+  { path:'iphone',component:IphoneComponent},
+  {path:'android',component:AndroidComponent},
+  {path:'help',component:HelpComponent},
+  {path:'signin',component:SigninComponent},
+  { path:'company',component:CompanyComponent},
+  {path:'**',component:PagenotfoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- netflix/src/app/app.module.ts
+++ netflix/src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { CommoncomponentsComponent } from './commoncomponents/commoncomponents.component';
+import { NavbarComponent } from './commoncomponents/navbar/navbar.component';
+import { FooterComponent } from './commoncomponents/footer/footer.component';
+import { AndroidComponent } from './navigatingcomponents/android/android.component';
+import { CompanyComponent } from './navigatingcomponents/company/company.component';
+import { HelpComponent } from './navigatingcomponents/help/help.component';
+import { IphoneComponent } from './navigatingcomponents/iphone/iphone.component';
+import { SigninComponent } from './navigatingcomponents/signin/signin.component';
+import { BannerComponent } from './welcome/banner/banner.component';
+import { LovedComponent } from './welcome/loved/loved.component';
+import { MobilesecurityComponent } from './welcome/mobilesecurity/mobilesecurity.component';
+import { LatestinsightsComponent } from './welcome/latestinsights/latestinsights.component';
+import { SpywareComponent } from './welcome/spyware/spyware.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    PagenotfoundComponent,
+    CommoncomponentsComponent,
+    NavbarComponent,
+    FooterComponent,
+    AndroidComponent,
+    CompanyComponent,
+    HelpComponent,
+    IphoneComponent,
+    SigninComponent,
+    BannerComponent,
+    LovedComponent,
+    MobilesecurityComponent,
+    LatestinsightsComponent,
+    SpywareComponent
   ],
   imports: [
     BrowserModule,
